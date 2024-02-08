0d58f269ed4bc5b5634f00fdc263c475d307b00e
Mirza <mirzaselimovic2005@gmail.com>
2024-02-08 12:45:48 +0100

    small update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ __('Websitenazorg+') }}</title>
+    <title>Websitenazorg+</title>
     <link rel="icon" href="{{ asset('Logo.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
@@ -15,7 +15,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    <img class="app-img" src="{{ asset('/Logo.png') }}" alt="{{ __('Logo of Websitenazorg') }}">
+                    <img class="app-img" src="{{ asset('/Logo.png') }}" alt="Logo van Websitenazorg">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -25,6 +25,7 @@
                     <ul class="navbar-nav me-auto">
                     </ul>
 
+                    <ul class="navbar-nav ms-auto">
                         @guest
                             @if (Route::has('register'))
                                 <li class="nav-item">
@@ -33,7 +34,7 @@
                             @endif
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="/domeinen">{{ __('Domains') }}</a> 
+                                <a class="nav-link" href="/domeinen">{{ __('Domeinen') }}</a> 
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -42,12 +43,12 @@
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/settings">
-                                        {{ __('Settings') }}
+                                        {{ __('Instellingen') }}
                                     </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Uitloggen') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
